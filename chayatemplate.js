b21481518c65476e3a1086c83e7a9c4795705c8e
chayabulan183 <35399658+chayabulan183@users.noreply.github.com>
2018-05-09 18:53:21 +0700

    Update chayatemplate.js

--- chayatemplate.js
+++ chayatemplate.js
@@ -132,7 +132,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://chayatemplate.blogspot.co.id/p/download.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://chayatemplate.blogspot.com/p/download.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
